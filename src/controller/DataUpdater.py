868f14f54c001ae2d68469fd2b9256ab740bd9bb
Banati Balazs <balazs.banati@gmail.com>
2024-05-01 20:26:30 +0200

    add new comment in DataUpdater.py

--- src/controller/DataUpdater.py
+++ src/controller/DataUpdater.py
@@ -25,83 +25,106 @@ class DataUpdater:
         return wrapper
 
     def update(self, current_player: Player, opponent_player: Player, board: Board):
-
-        """
-        When the game starts, first the players are initialized with their pieces
-
-        # GameController class: start_game() method ->
-        # Player class: init_pieces() method
-        ----------------------------------------------
-        Players are initialized with the following data:
-
-        1. self._pieces: ✔ list is filled with the pieces
-        2. self._piece_coordinates: ✔ set is filled with the coordinates of the pieces
-        3. self._selected_piece: x
-        4. self._last_moved_piece: x
-        5. self._all_possible_move: x
-        6. self._protected_fields: x
-        7. self._special_moves: x
-        8. self._attacked_fields: x
-
-        # DataUpdater class: update() method -> update_players() method ->
-        # Player class -> update_pieces_data() method
-        ----------------------------------------------
-        Pieces are updated with the following data:
-
-        - possible_fields: ✔
-        - protected_fields: ✔
-        - attacked_fields: ✔ (it is the same as possible_fields except for the pawns)
-
-        # Player: update_possible_moves_of_selected_piece() method
-        ----------------------------------------------
-        if not None:
-        Selected_piece got updated with the following data:
-
-        - possible_fields: ✔
-        - protected_fields: ✔
-        - attacked_fields: ✔
-
-        # Player: get_special_moves() method
-        ----------------------------------------------
-        En passant and castling moves are added to the special_moves list if they are possible.
-        resets the special_moves set and adds the special moves to it.
-
-        6. special_moves: ✔
-
-        # Player: update_protected_and_attacked_fields() method
-        ----------------------------------------------
-        Resets the protected_fields and attacked_fields sets and adds the new data to them.
-
-        5. protected_fields: ✔
-        7. attacked_fields: ✔
-
-        Summary:
-
-        Players are updated with the following data:
-
-        1. self._pieces: ✔
-        2. self._piece_coordinates: ✔
-        3. self._selected_piece: x
-        4. self._last_moved_piece: x
-        5. self._protected_fields: ✔
-        6. self._special_moves: ✔
-        7. self._attacked_fields: ✔
-
-        Pieces are updated with the following data:
-
-        - possible_fields: ✔
-        - protected_fields: ✔
-        - attacked_fields: ✔
-
-
-
-
-        """
-
         self.update_players(current_player, opponent_player, board)
         self.update_board(current_player, opponent_player, board)
 
     def update_players(self, current_player: Player, opponent_player: Player, board: Board):
+        """
+                When the game starts, first the players are initialized with their pieces
+
+
+
+                # GameController class: start_game() method ->
+                --------------------------------------------------------------------------------
+                |# Player class: init_pieces() method                                          |
+                --------------------------------------------------------------------------------
+                Players are initialized with the following data:
+
+                1. self._pieces: ✔ list is filled with the pieces
+                2. self._piece_coordinates: ✔ set is filled with the coordinates of the pieces
+                3. self._selected_piece: x
+                4. self._last_moved_piece: x
+                5. self._protected_fields: x
+                6. self._special_moves: x
+                7. self._attacked_fields: x
+
+
+
+
+                # DataUpdater class: update() method -> update_players() method ->
+                # Player class -> update_pieces_data() method
+                --------------------------------------------------------------------------------
+                |# Piece class -> update_piece() method                                        |
+                --------------------------------------------------------------------------------
+                :parameter: board -> Board class: 1. piece_board (below)
+
+                Pieces are updated with the following data:
+
+                - possible_fields: ✔
+                - protected_fields: ✔
+                - attacked_fields: ✔ (it is the same as possible_fields except for the pawns)
+
+
+
+
+                # Player: update_possible_moves_of_selected_piece() method
+                --------------------------------------------------------------------------------
+                |# Piece class -> update_piece() method                                        |
+                --------------------------------------------------------------------------------
+                :parameter: board -> Board class: 1. piece_board (below)
+
+                *** This method is redundant, after the update_pieces_data() method,
+                the selected piece is already updated. *** #TODO: Remove this method
+
+                if not None:
+                Selected_piece got updated with the following data:
+
+                - possible_fields: ✔
+                - protected_fields: ✔
+                - attacked_fields: ✔
+
+
+                --------------------------------------------------------------------------------
+                # Player: get_special_moves() method -> add_en_passant_to_special_moves() method
+                                                        add_castling_to_special_moves() method
+                --------------------------------------------------------------------------------
+                :parameter: last_moved_piece -> Piece class: 1. last_moved_piece
+
+                En passant and castling moves are added to the special_moves list if they are possible.
+                resets the special_moves set and adds the special moves to it.
+
+                6. special_moves: ✔
+
+
+                --------------------------------------------------------------------------------
+                # Player: update_protected_and_attacked_fields() method                        |
+                --------------------------------------------------------------------------------
+                Resets the protected_fields and attacked_fields sets and adds the new data to them.
+
+                5. protected_fields: ✔
+                7. attacked_fields: ✔
+
+                ===============================================================================
+                Summary:
+                ==============================================================================
+                Players are updated with the following data:
+
+                1. self._pieces: ✔
+                2. self._piece_coordinates: ✔
+                3. self._selected_piece: x
+                4. self._last_moved_piece: x
+                5. self._protected_fields: ✔
+                6. self._special_moves: ✔
+                7. self._attacked_fields: ✔
+
+                Pieces are updated with the following data:
+
+                - possible_fields: ✔
+                - protected_fields: ✔
+                - attacked_fields: ✔
+
+                ===============================================================================
+                """
         # 0. Update pieces data
         current_player.update_pieces_data()
         opponent_player.update_pieces_data()
@@ -120,6 +143,58 @@ class DataUpdater:
         opponent_player.update_protected_and_attacked_fields()
 
     def update_board(self, current_player: Player, opponent_player: Player, board: Board):
+
+        """
+        Board class:
+        1. piece_board: the positions of the pieces - ByteArray8x8
+        2. coloring_board: the coloring of the squares (view) - CharArray8x8
+        3. white attack board: the attacked fields by the white player - BoolArray8x8
+        4. black attack board: the attacked fields by the black player - BoolArray8x8
+        5. white protection board: the protected fields by the white player - BoolArray8x8
+        6. black protection board: the protected fields by the black player - BoolArray8x8
+
+
+        # DataUpdater class: update() method -> update_board() method ->
+        # Board class -> update_piece_board() method
+        ----------------------------------------------
+        1. piece_board: ✔
+            - parameters: white_player_pieces, black_player_pieces - Player class: 1. pieces
+
+        # Board class: update_attack_boards() method
+        ----------------------------------------------
+        3-4. white attack board, black attack board: ✔
+            - parameters: attacked_fields, attacked_by_black - Player class: 7. attacked_fields
+
+
+        # Board class: update_protection_boards() method
+        ----------------------------------------------
+        5-6. white protection board, black protection board: ✔
+            - parameters: protected_fields, protected_by_black - Player class: 5. protected_fields
+
+
+        # Board class: update_coloring_board() method
+        ----------------------------------------------
+        2. coloring_board: ✔
+            - parameters: selected_piece, special_moves - Player class: 3. selected_piece, 6. special_moves
+            ** The method only uses the coordinates of the selected piece and its possible moves. **
+
+        ==============================================
+        Summary:
+        ==============================================
+        Board is updated with the following data:
+
+        1. piece_board: ✔
+        2. coloring_board: ✔
+        3. white attack board: ✔
+        4. black attack board: ✔
+        5. white protection board: ✔
+        6. black protection board: ✔
+
+        ==============================================
+
+
+        """
+
         # It has the following boards:
         # 1. Piece board -> the positions of the pieces
         # 2. Coloring board -> the coloring of the squares (view)
@@ -142,6 +217,17 @@ class DataUpdater:
                                                  current_player.protected_fields)
 
         board.update_coloring_board(current_player.selected_piece, current_player.special_moves)
+
+    """
+    A better way to update the data:
+    
+    1. Initialize the Players with pieces
+    2. Update the piece_board with the current piece positions
+    3. Update the 
+    
+    """
+
+
 
     def get_data(self):
         return self.data
